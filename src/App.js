6be8f77b7e40c1fa9d8399cb67df027cdbea0bbc
Alexandre Morey <alexander006_9@hotmail.com>
2021-09-15 09:10:31 +0200

    react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <h2>And learn React!</h2>
+      <p>this is also visble</p>
+    </div>
+  );
+}
+
+export default App;
