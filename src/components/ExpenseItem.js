a0c8c7f951accad93e15f9157fb1949d0fe42f4f
Alexandre Morey <alexander006_9@hotmail.com>
2021-09-15 13:29:58 +0200

    expense items

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,6 +7,8 @@ function ExpenseItem(props) {
   return (
     <div className="expense-item">
       <div>{props.date.toISOString()}</div>
+      <div>{props.date.toLocaleString('en-US', {month: 'long'})}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
