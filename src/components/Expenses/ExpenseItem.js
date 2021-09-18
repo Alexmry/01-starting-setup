bb05f5d9197676a019dd0ce7a4a534c768fe42af
Alexandre Morey <alexander006_9@hotmail.com>
2021-09-18 16:42:27 +0200

    btn

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
@@ -7,16 +9,20 @@ function ExpenseItem(props) {
   // const expenseDate = new Date(2021, 2, 28);
   // const expenseTitle = 'Car insurance';
   // const expenseAmount = 294.67;
+  useState();
+
+  let title = props.title;
 
   const clickHandler = () => {
     console.log('clicked!');
+    title = 'updated';
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
