b9b85bad9e688c96cddaf29b751ec6ebdcd57642
Alexandre Morey <alexander006_9@hotmail.com>
2021-09-20 09:38:20 +0200

    Expenseform css file

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,3 +1,11 @@
-const ExpenseForm = () => {};
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+    return <form>
+        <div className='new-expense__controls'>
+            <div className='new-expense__control'></div>
+        </div>
+    </form>
+};
 
 export default ExpenseForm;
