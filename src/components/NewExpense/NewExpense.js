06c43aa381e9a67ca4ba4f5b398deeefa0597f4c
Alexandre Morey <alexander006_9@hotmail.com>
2021-09-20 09:31:41 +0200

    form css class

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,5 +1,12 @@
 import React from "react"; // you don't have to write that
+import "./NewExpense.css";
 
-const NewExpense = () => {};
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <form></form>
+    </div>
+  );
+};
 
 export default NewExpense;
